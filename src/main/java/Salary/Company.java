7f144a07cd576a7948541a5e4289b4e1df3d4fb1
JonatanPilichowski <108926973+JonatanPilichowski@users.noreply.github.com>
2022-08-24 22:37:56 +0200

    added menu

---
+++ src/main/java/Salary/Company.java
@@ -0,0 +1,12 @@
+package Salary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+    List<Employee> employees = new ArrayList<>();
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
